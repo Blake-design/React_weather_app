80ffd60a8bc19b67acb55612b2cbc562db645061
Blake McCarty <blake_mccarty@yahoo.com>
2022-05-05 23:34:36 -0500

    add path

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const api = require("./routes/index.js");
+const path = require("path");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 3001;
@@ -7,8 +8,16 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use("/", express.static("public"));
 app.use("/api", api);
+
+///// if in production send files to client build
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
